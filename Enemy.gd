94a9308ab350bb009ac5518ae3d147a8f920b2ae
Mystjerne <danaliewqie@gmail.com>
2024-09-06 04:03:49 +0800

    add knife, shotgun, enemy hp, staff and meat model 0. WeaponClass improved (now extends from MeshInstance3D, has damage and reload_spd properties) 1. Knife now has collision 2. When Knife comes into contact with the Skeleton enemy, it takes away enemy's health 3. Shotgun now has Raycast 4. When shotgun's Raycast is in contact with the Skeleton enemy and fired (left mouse click), it takes away enemy health 5. (Knife and Shotgun both player strength into consideration for damage dealt) 6. Skeleton has health label that changes when damage is dealt 7. staff and meat models added

--- Enemy.gd
+++ Enemy.gd
@@ -1,11 +1,26 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
-@export var health = 1000
+@export var health = 1000:
+	set = set_health
+@onready var health_label = $HealthLabel
 
+signal health_changed
 
 func _ready():
-	pass # Replace with function body.
+	health_label.text = "HP: " + str(health)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+# Setter function for health property
+func set_health(value):
+	# Check if the health is actually being changed
+	if health != value:
+		health = value
+		emit_signal("health_changed")
+
+# Handler for when health is changed
+func _on_health_changed():
+	print("HP:", health)
+	health_label.text = "HP: %s" % [str(health)]
