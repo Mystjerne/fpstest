5d22796a4037759221402662f031db89d4210652
Mystjerne <danaliewqie@gmail.com>
2024-09-07 04:20:04 +0800

    can enable/disable weapons in use/not in use 1. enable function added to all equipment types (melee, ranged, consumable and magic) 2. player script calls enable function when the user switches weapons using 1, 2, 3, or 4 3. disables weapons that are not currently in use. (disables their area3ds, raycasts, visibility etc)

--- Knife.gd
+++ Knife.gd
@@ -1,17 +1,19 @@
 extends Weapon
 
 @onready var animation_player = $AnimationPlayer
+@onready var knife_area3d = $Area3D
 
 func _ready():
-	damage = 10
+	damage = 1
+	reload_spd = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func enable(is_enabled:bool):
+	self.visible = is_enabled
+	knife_area3d.monitoring = is_enabled
 
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("mouse_left"):
 		animation_player.play("stab")
-
 
 func _on_area_3d_body_entered(body):
 	if body.name == "Skeleton":
